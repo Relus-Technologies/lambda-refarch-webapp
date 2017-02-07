2ccdff7908dcdd27ece9a60744a3345f0e90ddcc
Josh Stein <josh@reluscloud.com>
2017-02-07 15:29:17 -0500

    update with relusLogo and background gray

--- website/src/app/components/jumbotron/jumbotron.directive.js
+++ website/src/app/components/jumbotron/jumbotron.directive.js
@@ -20,7 +20,7 @@
 		return directive;
 
 		function link(scope,el,attrs) {
-			scope.img = attrs.img || 'assets/images/aws-cloud.png';
+			scope.img = attrs.img || 'assets/images/relusLogo.png';
 			scope.alt = attrs.apt || 'Amazon Web Services';
 			scope.title = attrs.title || 'Serverless Blog Engine';
 			scope.description = attrs.description || 'A serverless blog engine using Amazon Web Services'; 
